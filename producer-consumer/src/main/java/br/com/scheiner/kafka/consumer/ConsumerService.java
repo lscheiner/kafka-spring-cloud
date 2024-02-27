59bdf6ba82c6f4c3c3b97ce8bb734b3f3f04b44b
lscheiner <leandro.capinan@gmail.com>
2024-02-27 02:15:40 -0300

    comentarios

--- producer-consumer/src/main/java/br/com/scheiner/kafka/consumer/ConsumerService.java
+++ producer-consumer/src/main/java/br/com/scheiner/kafka/consumer/ConsumerService.java
@@ -10,6 +10,8 @@ import br.com.scheiner.Employee;
 public class ConsumerService {
 
 	// ack precisa ser manual , quando der erro em todas as tentativas ele commita a mensagem
+	// ocorre um delay para consumir a primeira mensangem de não fizer um shutdown graceful, porque o spring tenta
+	// dar rejoin no grupo porque não foi feito um consumer.close()
 	
 	@KafkaListener(topics = "topico-dest" , groupId="consumer-group-1")
     public void consume(Employee message , Acknowledgment acknowledgment) {
